536d999fdfd1e58702f68fa4f75b006949040b79
lipusipu44 <anilpatro044@gmail.com>
2022-02-22 08:14:14 +0530

    Added changes for auto id increment

--- pet-clinic-data/src/main/java/com/anilpatro044/springpetclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/anilpatro044/springpetclinic/services/map/VetServiceMap.java
@@ -26,7 +26,7 @@ public class VetServiceMap extends
 
     @Override
     public Vet save(Vet object) {
-        return super.save(object.getId(),object);
+        return super.save(object);
     }
 
     @Override
