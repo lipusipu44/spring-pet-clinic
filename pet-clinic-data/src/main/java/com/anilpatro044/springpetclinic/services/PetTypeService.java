83aa1b284ac00ffbac9051ed4e91d7c9dd996de5
lipusipu44 <anilpatro044@gmail.com>
2022-02-23 11:53:42 +0530

    added PetTypeService and map

---
+++ pet-clinic-data/src/main/java/com/anilpatro044/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,7 @@
+package com.anilpatro044.springpetclinic.services;
+
+import com.anilpatro044.springpetclinic.model.Owner;
+import com.anilpatro044.springpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType,Long>{
+}
