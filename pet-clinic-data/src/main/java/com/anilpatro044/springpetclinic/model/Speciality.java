a32f2148ca6a3e8865d002f5e2de9885dce055b5
lipusipu44 <anilpatro044@gmail.com>
2022-03-02 09:28:29 +0530

    Converted Vet and Specialities to JPA from Pojo

--- pet-clinic-data/src/main/java/com/anilpatro044/springpetclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/anilpatro044/springpetclinic/model/Speciality.java
@@ -1,6 +1,13 @@
 package com.anilpatro044.springpetclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
